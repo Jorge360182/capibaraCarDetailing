cb4d1bf98a689e4dda77c76fed91b72db60df245
Jorge Eduardo Herrera Garcia <122805743+Jorge360182@users.noreply.github.com>
2025-02-04 14:17:26 -0600

    menuFijo.js

--- menuFijo.js
+++ menuFijo.js
@@ -1,36 +1,36 @@
-// (function () {
-//     let navbar = document.querySelector('#navbar');
-//     let main = document.querySelector('main');
-//     let hamburger = document.querySelector('#hamburger');
-//     let navbarHeight = navbar.getBoundingClientRect().height;
-//     let breakpoint = main.offsetTop - navbarHeight;
-//     let prevScrollPos = window.scrollY;
-//     let isFixed = false;
+(function () {
+    let navbar = document.querySelector('#navbar');
+    let main = document.querySelector('main');
+    let hamburger = document.querySelector('#hamburger');
+    let navbarHeight = navbar.getBoundingClientRect().height;
+    let breakpoint = main.offsetTop - navbarHeight;
+    let prevScrollPos = window.scrollY;
+    let isFixed = false;
 
-//     function onScroll() {
-//         let currentScrollPos = window.scrollY;
+    function onScroll() {
+        let currentScrollPos = window.scrollY;
 
-//         if (currentScrollPos >= breakpoint && !isFixed) {
-//             navbar.classList.remove('open');
-//             navbar.classList.add('navbar-fixed');
-//             isFixed = true;
-//         } else if (currentScrollPos < breakpoint && isFixed) {
-//             navbar.classList.remove('navbar-fixed');
-//             hamburger.classList.remove('open');
-//             isFixed = false;
-//         }
+        if (currentScrollPos >= breakpoint && !isFixed) {
+            navbar.classList.remove('open');
+            navbar.classList.add('navbar-fixed');
+            isFixed = true;
+        } else if (currentScrollPos < breakpoint && isFixed) {
+            navbar.classList.remove('navbar-fixed');
+            hamburger.classList.remove('open');
+            isFixed = false;
+        }
 
-//         if (currentScrollPos > prevScrollPos && isFixed) {
-//             navbar.classList.remove('open');
-//             navbar.classList.add('navbar-hidden');
-//             hamburger.classList.remove('open');
-//         } else if (currentScrollPos < prevScrollPos && isFixed && currentScrollPos > breakpoint) {
-//             navbar.classList.remove('navbar-hidden');
+        if (currentScrollPos > prevScrollPos && isFixed) {
+            navbar.classList.remove('open');
+            navbar.classList.add('navbar-hidden');
+            hamburger.classList.remove('open');
+        } else if (currentScrollPos < prevScrollPos && isFixed && currentScrollPos > breakpoint) {
+            navbar.classList.remove('navbar-hidden');
             
-//         }
+        }
 
-//         prevScrollPos = currentScrollPos;
-//     }
+        prevScrollPos = currentScrollPos;
+    }
 
-//     document.addEventListener('scroll', onScroll);
-// })();
+    document.addEventListener('scroll', onScroll);
+})();
