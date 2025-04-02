f6ea604c2be3a58b13b07f690f0e2f2d560cf710
Jorge Eduardo Herrera Garcia <122805743+Jorge360182@users.noreply.github.com>
2025-04-02 12:17:02 -0600

    tactiles.js

--- tactiles.js
+++ tactiles.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     const container = document.getElementById('autos-container');
 
     try {
-        const response = await fetch('/assets/data/autos.json');
+        const response = await fetch('autos.json');
         const autos = await response.json();
 
         if (autos.length === 0) {
